5e099a2f6d9b6e3bfaf7e3b1c3956010e094186a
Henry-Joseph Audéoud <henry-joseph.audeoud@univ-grenoble-alpes.fr>
2018-12-01 17:00:20 +0100

    Drop jinja requirement
    We embed the BIB entry format in the Python code.  This is a small
    format that are easily understood in this short script, and this allow
    us to don't require the jinja template tool anymore.

--- rfc2bib.py
+++ rfc2bib.py
@@ -2,8 +2,18 @@
 URL_RFC_XML = 'http://www.rfc-editor.org/in-notes/rfc-index.xml'
 
 from lxml import etree
-import jinja2
 import requests
+
+BIB_ENTRY_FORMAT = """\
+@MISC{%(key)s,
+	author = {%(author)s},
+	title = {%(title)s},
+	month = {%(month)s},
+	year = {%(year)s},
+	url = {%(url)s},
+	note = {%(key)s}
+}
+"""
 
 def tag_prefix(s):
     return '{http://www.rfc-editor.org/rfc-index}' + s
@@ -15,13 +25,6 @@ def normalize_authors(authors):
     authors = filter(lambda a: a != '', map(lambda a: a.strip(), authors))
     authors_reversed = map(lambda a: a.split()[-1] + ', ' + ' '.join(a.split()[0:-1]), authors)
     return ' and '.join(authors_reversed)
-
-# ref: https://raw.github.com/hupili/tutorial/master/README.tpl.md
-#      https://github.com/hupili/tutorial/blob/master/render_readme.py
-#
-template = jinja2.Template(open('bib-entry.tpl').read())
-def render_bib(data):
-    return template.render(data)
 
 index = etree.fromstring(requests.get(URL_RFC_XML).content)
 rfcs = index.findall(tag_prefix('rfc-entry'))
@@ -39,4 +42,4 @@ for r in rfcs:
     d['month'] = r.find(tag_prefix('date')).find(tag_prefix('month')).text
     d['url'] = 'http://tools.ietf.org/rfc/%s.txt' % d['key'].lower()
     #d['obsoletes'] = ','.join([o.find('doc-id').text for o in r.findall(tag_prefix('obsoletes'))])
-    print(render_bib(d))
+    print(BIB_ENTRY_FORMAT % d)
